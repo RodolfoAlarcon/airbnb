ddafa7c93c41a1b8d5c4a09ed5d1b9b5d8d48537
Rodolfo <ravnzl99mail.com>
2024-08-13 12:10:29 -0400

    primeros cambios
    listo

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <>hola</>
+  );
+}
+
+export default App;
---
+++ src/Components/ui/TopMenuEscritorio/TopMenuEscritorio.jsx
@@ -0,0 +1,5 @@
+export const TopMenuEscritorio = () => {
+  return (
+    <div>TopMenuEscritorio</div>
+  )
+}
